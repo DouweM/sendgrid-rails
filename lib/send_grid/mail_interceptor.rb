69a311583ea094f89805d7c5408b44f9e0489734
Douwe Maan <douwe@selenight.nl>
2014-08-27 20:54:08 +0200

    Only use X-SMTPAPI to field if necessary.

--- lib/send_grid/mail_interceptor.rb
+++ lib/send_grid/mail_interceptor.rb
@@ -2,9 +2,13 @@ module SendGrid
   class MailInterceptor
     def self.delivering_email(mail)
       sendgrid_header = mail.instance_variable_get(:@sendgrid_header)
-      sendgrid_header.add_recipients(mail.to)
+
+      if sendgrid_header.data.has_key?(:to) && sendgrid_header.data[:to].present?
+        sendgrid_header.add_recipients(mail.to)
+        mail.header['to'] = SendGrid.dummy_address if SendGrid.dummy_address
+      end
+      
       mail.header['X-SMTPAPI'] = sendgrid_header.to_json if sendgrid_header.data.present?
-      mail.header['to'] = SendGrid.dummy_address if SendGrid.dummy_address
     end
   end
 end
